9c8612ed737392cb8182db3f4f94e212a8b0d073
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-07-02 22:08:47 -0500

    Broken Pipe Error

--- prime_function/prime_client.py
+++ prime_function/prime_client.py
@@ -1,14 +1,17 @@
 import grpc
-from gen_grpc import oprc_offload_pb2, oprc_offload_pb2_grpc
+from gen_grpc import oprc_offload_pb2, oprc_offload_pb2_grpc, oprc_object_pb2
 import asyncio
 import logging
+import json
+
 
 async def run():
     async with grpc.aio.insecure_channel("localhost:50051") as channel:
         stub = oprc_offload_pb2_grpc.OTaskExecutorStub(channel)
+        data = {'PRIME': 5}
         request = oprc_offload_pb2.ProtoOTask(
             funcKey='example.prime.generate',
-            
+            main=oprc_object_pb2.ProtoOObject(data=json.dumps(data).encode('utf-8'))
         )
         response = await stub.invoke(request)
         print(response.main.data.decode('utf-8'))
--- prime_function/prime_server.py
+++ prime_function/prime_server.py
@@ -9,6 +9,7 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 level = logging.getLevelName(LOG_LEVEL)
 logging.basicConfig(level=level)
 
+
 def is_prime(n):
     # Check if a number is prime
     if n <= 1:
@@ -24,19 +25,21 @@ def is_prime(n):
         i += 6
     return True
 
+
 def next_prime(p):
     # Return the next prime number after the given prime p
     if not is_prime(p):
         raise ValueError(f"The input is not a prime number")
-    
+
     next_candidate = p + 1
     while not is_prime(next_candidate):
         next_candidate += 1
     return next_candidate
 
+
 class GrpcCtx:
-    main_data = 5
-    output_data = main_data
+    # main_data = 5
+    # output_data = main_data
 
     def __init__(self, request):
         self.task = request
@@ -44,16 +47,19 @@ class GrpcCtx:
         self.main_data = request.main.data
         self.output_data = request.output.data
 
+
 class PrimeHandler:
     # User's handler to generate prime numbers
-    async def handle(self, ctx:GrpcCtx):
-        prev_prime = json.loads(ctx.main_data).encode('utf-8')
+    async def handle(self, ctx: GrpcCtx):
+        data_in_dict = json.loads(ctx.main_data.decode('utf-8'))
+        prev_prime = int(data_in_dict.get('PRIME', 5))
         prime = next_prime(prev_prime)
-        ctx.main_data = prime
-        ctx.output_data = prime
+        new_data = {'PRIME': prime}
+        ctx.main_data = json.dumps(new_data).encode('utf-8')
+
 
 class OTaskExecutorServicer(oprc_offload_pb2_grpc.OTaskExecutorServicer):
-    def __init__(self, handler: PrimeHandler):
+    def __init__(self, handler: PrimeHandler()):
         self.prime_handler = handler
 
     async def invoke(self, request, context):
@@ -65,7 +71,7 @@ class OTaskExecutorServicer(oprc_offload_pb2_grpc.OTaskExecutorServicer):
             context.set_code(grpc.StatusCode.NOT_FOUND)
             context.set_details('Handler not found')
             return oprc_offload_pb2.ProtoOTaskCompletion(id=request.id, success=False)
-        
+
         main_obj = json.dumps(ctx.main_data).encode('utf-8')
         if ctx.output_data is not None:
             output_obj = json.dumps(ctx.output_data).encode('utf-8')
@@ -79,6 +85,7 @@ class OTaskExecutorServicer(oprc_offload_pb2_grpc.OTaskExecutorServicer):
             output=oprc_offload_pb2.ProtoObjectUpdate(data=output_obj)
         )
 
+
 async def serve():
     server = grpc.aio.server()
     prime_handler = PrimeHandler()
@@ -88,6 +95,7 @@ async def serve():
     logging.info("Starting server on %s", listen_addr)
     await server.start()
     await server.wait_for_termination()
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
