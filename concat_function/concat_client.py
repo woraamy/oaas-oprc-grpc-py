69e8c88b1605e70043461428adc5b5461eb519cc
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-07-04 22:40:12 -0500

    Try it with grpc

---
+++ concat_function/concat_client.py
@@ -0,0 +1,23 @@
+import json
+
+import grpc
+from gen_grpc import oprc_offload_pb2, oprc_offload_pb2_grpc, oprc_object_pb2
+import asyncio
+import logging
+
+
+async def run():
+    async with grpc.aio.insecure_channel("localhost:50052") as channel:
+        stub = oprc_offload_pb2_grpc.FunctionExecutorStub(channel)
+        data = {"helllooo": "world"}
+        request = oprc_offload_pb2.ProtoOTask(
+            funcKey='example.record.random',
+            main=oprc_object_pb2.ProtoPOObject(data=json.dumps(data).encode('utf-8'))
+        )
+        response = await stub.invoke(request)
+        print(response)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    asyncio.run(run())
