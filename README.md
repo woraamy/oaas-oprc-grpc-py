4b8c4c927dfc832e419b4396a6ba87b0c1178a6c
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-07-11 20:18:56 -0500

    Upload package to pypi

---
+++ README.md
@@ -0,0 +1,3 @@
+# GRPC SDK for OaaS
+This SDK is generated from GRPC proto files for OaaS. 
+It provides a way to interact with OaaS services.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="oaas-sdk-grpc",
+    version="0.0.1",
+    author="Worawalan Chatlatanagulchai",
+    author_email="worawalanc@gmail.com",
+    description="GRPC SDK for OAAS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/woraamy/oaas-oprc-grpc-py",
+    packages=find_packages(include=["oaas_sdk_grpc", "oaas_sdk_grpc.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
