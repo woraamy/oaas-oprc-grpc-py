93b8dbacdc4ab10a4d07a130c50f165b29723bd9
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-07-16 18:27:07 -0500

    Remove the test files

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="oaas-sdk-grpc",
-    version="0.0.1",
+    version="0.0.2",
     author="Worawalan Chatlatanagulchai",
     author_email="worawalanc@gmail.com",
     description="GRPC SDK for OAAS",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/woraamy/oaas-oprc-grpc-py",
-    packages=find_packages(include=["oaas_sdk_grpc", "oaas_sdk_grpc.*", "gen_grpc", "gen_grpc.*"]),
+    packages=find_packages(include=["oaas_sdk_grpc", "oaas_sdk_grpc.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
