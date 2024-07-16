92e38bcad1ac91344db482dcdac1c9b32a2f9aa2
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-07-16 18:46:12 -0500

    Add init file to gen_grpc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="oaas-sdk-grpc",
-    version="0.0.2",
+    version="0.0.3",
     author="Worawalan Chatlatanagulchai",
     author_email="worawalanc@gmail.com",
     description="GRPC SDK for OAAS",
